b3c009e7c10ed2442a20730c25bab3bb5ab8d5de
solidsnakedev <jona.ca.eng@gmail.com>
2024-03-03 14:03:07 -0500

    feat: add attachScript, mintAssets, validFrom, validTo to makeTx function

--- src/tx-builder/MakeTx.ts
+++ src/tx-builder/MakeTx.ts
@@ -1,6 +1,14 @@
-import { attachScript, attachSpendingValidator } from "./Attach.js";
+import {
+  attachCertificateValidator,
+  attachMintingPolicy,
+  attachScript,
+  attachSpendingValidator,
+  attachWithdrawalValidator,
+} from "./Attach.js";
 import { collectFrom } from "./Collect.js";
 import { complete } from "./Complete.js";
+import { validFrom, validTo } from "./Interval.js";
+import { mintAssets } from "./Mint.js";
 import { payToAddress, payToAddressWithData } from "./Pay.js";
 import { readFrom } from "./Read.js";
 import { Config } from "./types.js";
@@ -9,10 +17,16 @@ export function makeTx(config: Config) {
   return {
     readFrom: readFrom(config),
     collectFrom: collectFrom(config),
-    attachSpendingValidator: attachSpendingValidator(config),
     attachScript: attachScript(config),
+    attachSpendingValidator: attachSpendingValidator(config),
+    attachMintingPolicy: attachMintingPolicy(config),
+    attachCertificateValidator: attachCertificateValidator(config),
+    attachWithdrawalValidator: attachWithdrawalValidator(config),
     payToAddress: payToAddress(config),
     payToAddressWithData: payToAddressWithData(config),
+    mintAssets: mintAssets(config),
+    validFrom: validFrom(config),
+    validTo: validTo(config),
     complete: complete(config),
     config: () => config,
   };
