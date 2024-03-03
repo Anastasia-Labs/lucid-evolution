2b3a5518def56b4645266de88ae9dce208056084
solidsnakedev <jona.ca.eng@gmail.com>
2024-03-03 08:41:23 -0500

    feat: add toCMLTransactionHash as utility function

--- src/lucid/lucid.ts
+++ src/lucid/lucid.ts
@@ -43,6 +43,7 @@ import {
 import { Message } from "./message.js";
 import { Tx } from "./tx.js";
 import { TxComplete } from "./tx_complete.js";
+import { toCMLTransactionHash } from "../tx-builder/utils.js";
 
 export class Lucid {
   txBuilderConfig!: CML.TransactionBuilderConfig;
@@ -524,13 +525,10 @@ export class Lucid {
           // console.log("txHash CSL", txhash.to_hex())
           // const lucidTxHash = C.hash_transaction(C.TransactionBody.from_bytes(fromHex("a500838258200d31e3060edc0422bab792b414b6920534fb61f72a24cb76c911fea670601518008258205a4a961c2ff22b870f06f5303dcfffaaf3afdb819b12f5f0e60f0540e0e7490700825820f9178e7551be72eb512f8b1b739346c84e9cda4ef8be4feada9bfff6fe47e80000018182583900bc83d1474fafc0669e360c6ef389d3874f5eff99c31f343ff2cdab0bcb68eb5507f1a2a0cb4ddb095125f0f9878568cfe2466f2969db1ed3821a02050c95a9581c748a7026de606c27c06e4b90cb2e1844b566b89fba0d282debe4b40aa14d4d794d696e746564546f6b656e01581ca11c0e63717ac9d8d7bd50c6b29111003e765d199915a02613069641a14d4d794d696e746564546f6b656e181e581ca49d85bfbd193446051e644e7ad448682a1bc1b02d5a15f78429a1f0a14d4d794d696e746564546f6b656e0a581cc7e5e285a6ca1fdd6b8b0293b4d825a9c92ae69f8fc35ae2252a697da14d4d794d696e746564546f6b656e0a581cfed7a8ee8af612b08a05304ba434ef18f37be06f144fbc190d377942a14d4d794d696e746564546f6b656e0a581c92db8682a9e00473968130a7d3ee6d1bd331812543a8584ba739c74ca14d4d794d696e746564546f6b656e01581cd3c9b677c3e599ac2eb0a455e4d5d027d5ef860caf72b5dc27ba0e16a14d4d794d696e746564546f6b656e01581ce2085cebe2ce09d4ec0cbfe965f332941686c9288199440d45e95ff0a14d4d794d696e746564546f6b656e01581c2fce0a65bac1267022f94296e3d55fb862979b13bed38521a31cd200a14d4d794d696e746564546f6b656e01021a0002fb89031a0333a0f509a1581c2fce0a65bac1267022f94296e3d55fb862979b13bed38521a31cd200a14d4d794d696e746564546f6b656e01")))
           // console.log("lucidTxHash", lucidTxHash.to_hex())
-          const TransactionHash = C.hash_transaction(
-            C.TransactionBody.from_bytes(fromHex(tx.body().to_cbor_hex()))
-          );
 
           const witness = CML.make_vkey_witness(
             // CML.hash_transaction(tx.body()),
-            CML.TransactionHash.from_hex(TransactionHash.to_hex()),
+            toCMLTransactionHash(tx.body()),
             priv
           );
           txWitnessSetBuilder.add_vkey(witness);
