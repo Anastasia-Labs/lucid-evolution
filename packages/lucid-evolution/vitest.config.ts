0278212382b7a77d51c54024ddcc57a958174529
solidsnakedev <jona.ca.eng@gmail.com>
2024-03-21 11:21:06 -0400

    test: downgrade vitest

--- packages/lucid-evolution/vitest.config.ts
+++ packages/lucid-evolution/vitest.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    poolOptions: {
-      threads: {
-        isolate: true,
-        useAtomics: true,
-        // Threads related options here
-      },
-    },
     reporters: "verbose",
     include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
