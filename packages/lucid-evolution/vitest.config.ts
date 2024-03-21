e2368268754f8c55b5deebae3d0f984d249593ec
solidsnakedev <jona.ca.eng@gmail.com>
2024-03-21 10:59:18 -0400

    test: update vitest config

--- packages/lucid-evolution/vitest.config.ts
+++ packages/lucid-evolution/vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    poolOptions: {
+      threads: {
+        isolate: true,
+        useAtomics: true
+        // Threads related options here
+      },
+    },
     reporters: "verbose",
     include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
