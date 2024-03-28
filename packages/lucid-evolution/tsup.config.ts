27ad3a98bd3a7d3b5580e5622bd1834a92965409
solidsnakedev <jona.ca.eng@gmail.com>
2024-03-28 10:56:43 -0400

    build: add wasm support to vitest and tsup; add cborg package

---
+++ packages/lucid-evolution/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+import { wasmLoader } from 'esbuild-plugin-wasm'
+
+export default defineConfig({
+  esbuildPlugins: [wasmLoader()],
+  format: ["esm" ],
+  dts: true,
+  clean: true
+})
