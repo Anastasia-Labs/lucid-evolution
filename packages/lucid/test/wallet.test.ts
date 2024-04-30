4e86d3b43398654dc67e6bcafe13502908511870
solidsnakedev <jona.ca.eng@gmail.com>
2024-04-30 16:32:14 -0400

    refactor: improve struct

--- packages/lucid/test/wallet.test.ts
+++ packages/lucid/test/wallet.test.ts
@@ -5,21 +5,27 @@ import {
   Lucid,
   Maestro,
 } from "../src/index.js";
-import { Config, Console, Effect, pipe } from "effect";
+import { Config, Effect } from "effect";
+
+const loadConfig = Effect.gen(function* () {
+  return yield* Config.all([
+    Config.string("VITE_API_URL"),
+    Config.string("VITE_BLOCKFROST_KEY"),
+    Config.string("VITE_SEED"),
+    Config.string("VITE_MAESTRO_KEY"),
+  ]);
+});
 
 describe("Wallet", () => {
   test("switchProvider", async () => {
-    const program = Effect.gen(function* () {
+    Effect.gen(function* () {
       const [VITE_API_URL, VITE_BLOCKFROST_KEY, VITE_SEED, VITE_MAESTRO_KEY] =
-        yield* Config.all([
-          Config.string("VITE_API_URL"),
-          Config.string("VITE_BLOCKFROST_KEY"),
-          Config.string("VITE_SEED"),
-          Config.string("VITE_MAESTRO_KEY"),
-        ]);
+        yield* loadConfig;
+
       const user = yield* Effect.tryPromise(() =>
         Lucid(new Blockfrost(VITE_API_URL, VITE_BLOCKFROST_KEY), "Preprod"),
       );
+
       user.selectWallet.fromSeed(VITE_SEED);
 
       const blockfrostUTXO = yield* Effect.promise(() =>
@@ -31,15 +37,8 @@ describe("Wallet", () => {
       });
       yield* Effect.tryPromise(() => user.switchProvider(maestro));
       const maestroUTXO = yield* Effect.promise(() => user.wallet().getUtxos());
-      yield* pipe(
-        Effect.tryPromise(() => maestro.getProtocolParameters()),
-        Effect.tap(Console.log),
-      );
-      yield* Console.log(blockfrostUTXO);
-      yield* Console.log(maestroUTXO);
-    }).pipe(Effect.tapErrorCause(Console.log));
-    const exit = await Effect.runPromiseExit(program);
-    expect(exit._tag).toBe("Success");
+      assert.deepStrictEqual(blockfrostUTXO, maestroUTXO);
+    }).pipe(Effect.runPromise);
   });
 
   test("generateSeedPhrase", async () => {
