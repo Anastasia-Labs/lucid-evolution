0ca6164c7ddb11c8657f8a479eb8f0dbc91a96fe
solidsnakedev <jona.ca.eng@gmail.com>
2024-04-30 14:38:38 -0400

    fix: update wallet testing

--- packages/lucid/test/wallet.test.ts
+++ packages/lucid/test/wallet.test.ts
@@ -1,31 +1,46 @@
-import { assert, test } from "vitest";
-import { Blockfrost, Lucid, Maestro } from "../src/index.js";
+import { assert, describe, expect, test } from "vitest";
+import {
+  Blockfrost,
+  generateSeedPhrase,
+  Lucid,
+  Maestro,
+} from "../src/index.js";
+import { Config, Effect } from "effect";
 
-let setupFail = false;
-const maestro = new Maestro({
-  apiKey: process.env.VITE_MAESTRO_KEY!,
-  network: "Preprod",
-});
-try {
-  const protocol = await maestro.getProtocolParameters();
-  // console.log(protocol);
-} catch (error) {
-  console.log();
-  setupFail = true;
-}
+describe("Wallet", () => {
+  test("switchProvider", async () => {
+    const program = Effect.gen(function* () {
+      const [VITE_API_URL, VITE_BLOCKFROST_KEY, VITE_SEED, VITE_MAESTRO_KEY] =
+        yield* Config.all([
+          Config.string("VITE_API_URL"),
+          Config.string("VITE_BLOCKFROST_KEY"),
+          Config.string("VITE_SEED"),
+          Config.string("VITE_MAESTRO_KEY"),
+        ]);
+      const user = yield* Effect.tryPromise(() =>
+        Lucid(new Blockfrost(VITE_API_URL, VITE_BLOCKFROST_KEY), "Preprod")
+      );
+      user.selectWallet.fromSeed(VITE_SEED);
 
-test.skip("test wallet-provider", async () => {
-  const user = await Lucid(
-    new Blockfrost(process.env.VITE_API_URL!, process.env.VITE_BLOCKFROST_KEY),
-    "Preprod",
-  );
-  user.selectWallet.fromSeed(process.env.VITE_SEED!);
+      const blockfrostUTXO = yield* Effect.promise(() =>
+        user.wallet().getUtxos()
+      );
+      const maestro = new Maestro({
+        apiKey: VITE_MAESTRO_KEY,
+        network: "Preprod",
+      });
+      yield* Effect.tryPromise(() => user.switchProvider(maestro));
+      const maestroUTXO = yield* Effect.promise(() => user.wallet().getUtxos());
+      assert.deepStrictEqual(blockfrostUTXO, maestroUTXO);
+    });
+    const exit = await Effect.runPromiseExit(program);
+    expect(exit._tag).toBe("Success");
+  });
 
-  const blockfrostUTXO = await user.wallet().getUtxos();
-
-  await user.switchProvider(maestro);
-
-  const maestroUTXO = await user.wallet().getUtxos();
-
-  assert.deepStrictEqual(blockfrostUTXO, maestroUTXO);
+  test("generateSeedPhrase", async () => {
+    const seed = generateSeedPhrase();
+    assert(seed);
+    assert.equal(seed.split(" ").length, 24);
+    assert.notEqual(generateSeedPhrase(), seed);
+  });
 });
