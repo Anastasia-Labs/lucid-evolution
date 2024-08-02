f0bf16db7d043696e31eb1196fc9cf2076fb7ae2
solidsnakedev <jona.ca.eng@gmail.com>
2024-08-02 07:58:22 -0600

    feat: add assemble tx ; partial sign; toJSON; toCBOR; toHash

--- packages/lucid/src/tx-submit/TxSubmit.ts
+++ packages/lucid/src/tx-submit/TxSubmit.ts
@@ -1,16 +1,25 @@
 import { Effect } from "effect";
+import * as S from "@effect/schema/Schema";
 import { CML, makeReturn } from "../core.js";
 import { Wallet } from "@lucid-evolution/core-types";
 import { Either } from "effect/Either";
 import { TxSubmitError } from "../Errors.js";
+import { CBORHex, Hash } from "../tx-builder/types.js";
 
-export type TxSigned = {
+export interface TxSigned {
+  /** Submits the transaction and returns the transaction hash. */
   submit: () => Promise<string>;
+  /** Submits the transaction and returns the transaction hash or error as an effect. */
   submitProgram: () => Effect.Effect<string, TxSubmitError, never>;
+  /** Safely submits the transaction, returning the transaction hash or an error as promise either type. */
   submitSafe: () => Promise<Either<string, TxSubmitError>>;
-  toCBOR: () => string;
-  toHash: () => string;
-};
+  /** Converts the transaction (transaction body and witnesses) to CBOR format. */
+  toCBOR: () => CBORHex;
+  /** Converts the transaction (transaction body and witnesses) to JSON format. */
+  toJSON: () => object;
+  /** Computes the hash of the transaction. */
+  toHash: () => Hash;
+}
 export const makeSubmit = (
   wallet: Wallet,
   txSigned: CML.Transaction,
@@ -21,9 +30,11 @@ export const makeSubmit = (
   });
   return {
     submit: () => makeReturn(submit).unsafeRun(),
-    submitProgram: () => makeReturn(submit).program(),
+    submitProgram: () => submit,
     submitSafe: () => makeReturn(submit).safeRun(),
     toCBOR: () => txSigned.to_cbor_hex(),
+    toJSON: () =>
+      S.decodeUnknownSync(S.parseJson(S.Object))(txSigned.to_json()),
     toHash: () => CML.hash_transaction(txSigned.body()).to_hex(),
   };
 };
