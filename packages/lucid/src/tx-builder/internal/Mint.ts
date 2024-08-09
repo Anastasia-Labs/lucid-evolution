ca7d1f5565c7c128d4d79840cbdf37f8e7d149de
solidsnakedev <jona.ca.eng@gmail.com>
2024-08-09 15:53:29 -0600

    fix: format

--- packages/lucid/src/tx-builder/internal/Mint.ts
+++ packages/lucid/src/tx-builder/internal/Mint.ts
@@ -33,42 +33,42 @@ export const mintAssets =
       const policy = yield* pipe(
         Effect.fromNullable(config.scripts.get(policyId)),
         Effect.orElseFail(() =>
-          mintError(ERROR_MESSAGE.MISSING_POLICY(policyId))
-        )
+          mintError(ERROR_MESSAGE.MISSING_POLICY(policyId)),
+        ),
       );
       switch (policy.type) {
         case "Native":
           config.txBuilder.add_mint(
             mintBuilder.native_script(
               CML.NativeScript.from_cbor_hex(policy.script),
-              CML.NativeScriptWitnessInfo.assume_signature_count()
-            )
+              CML.NativeScriptWitnessInfo.assume_signature_count(),
+            ),
           );
           break;
 
         case "PlutusV1": {
           const red = yield* pipe(
             Effect.fromNullable(redeemer),
-            Effect.orElseFail(() => mintError(ERROR_MESSAGE.MISSING_REDEEMER))
+            Effect.orElseFail(() => mintError(ERROR_MESSAGE.MISSING_REDEEMER)),
           );
           config.txBuilder.add_mint(
             mintBuilder.plutus_script(
               toPartial(toV1(policy.script), red),
-              CML.Ed25519KeyHashList.new()
-            )
+              CML.Ed25519KeyHashList.new(),
+            ),
           );
           break;
         }
         case "PlutusV2": {
           const red = yield* pipe(
             Effect.fromNullable(redeemer),
-            Effect.orElseFail(() => mintError(ERROR_MESSAGE.MISSING_REDEEMER))
+            Effect.orElseFail(() => mintError(ERROR_MESSAGE.MISSING_REDEEMER)),
           );
           config.txBuilder.add_mint(
             mintBuilder.plutus_script(
               toPartial(toV2(policy.script), red),
-              CML.Ed25519KeyHashList.new()
-            )
+              CML.Ed25519KeyHashList.new(),
+            ),
           );
           break;
         }
