de9b6720f8052dfc01c77e315fec8fa838204d82
nikhils9 <nikhilshetty447@gmail.com>
2024-08-28 18:33:20 +0530

    fix: missing CML export

--- packages/lucid/src/index.ts
+++ packages/lucid/src/index.ts
@@ -2,6 +2,7 @@ export * from "./lucid-evolution/index.js";
 export * from "./tx-builder/index.js";
 export * from "./tx-sign-builder/index.js";
 export * from "./Errors.js";
+export * from "./core.js";
 export * from "@lucid-evolution/core-types";
 export * from "@lucid-evolution/core-utils";
 export * from "@lucid-evolution/plutus";
