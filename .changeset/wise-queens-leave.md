dd3bd772f59fc774c32b140e1e06314df2c69e4b
nikhils9 <nikhilshetty447@gmail.com>
2024-12-13 14:23:23 +0530

    chore: add changeset

---
+++ .changeset/wise-queens-leave.md
@@ -0,0 +1,6 @@
+---
+"@lucid-evolution/lucid": patch
+"@lucid-evolution/utils": patch
+---
+
+Fix `unixTimeToSlot` for Custom network
--- packages/utils/src/time.ts
+++ packages/utils/src/time.ts
@@ -6,12 +6,12 @@ import {
 } from "@lucid-evolution/plutus";
 
 /**
- * Converts unix time to slot based on the network. For "Custom" network 
+ * Converts unix time to slot based on the network. For "Custom" network
  * it is advisable use `unixTimeToSlot` method from `LucidEvolution`
  * instance to avoid uninitialized `SLOT_CONFIG_NETWORK` issue. More details
  * on the issue can be found here https://github.com/Anastasia-Labs/lucid-evolution/pull/443
- * @param network 
- * @param unixTime 
+ * @param network
+ * @param unixTime
  * @returns Slot
  */
 export function unixTimeToSlot(network: Network, unixTime: UnixTime): Slot {
