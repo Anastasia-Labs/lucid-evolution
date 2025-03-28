34dd63e2391f3d90f0e9cfe46543a9e6d9b59045
solidsnakedev <jona.ca.eng@gmail.com>
2025-03-28 08:07:00 -0600

    build: fix lint

--- packages/lucid/tsup.config.ts
+++ packages/lucid/tsup.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "tsup";
-import { wasmLoader } from "esbuild-plugin-wasm";
 
 export default defineConfig({
-  esbuildPlugins: [wasmLoader()],
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
