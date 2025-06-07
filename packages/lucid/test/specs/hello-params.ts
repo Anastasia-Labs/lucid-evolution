b66c30d349799f1f6c64f956c5000b14f1df3c68
hade <liveithade@gmail.com>
2025-06-07 11:08:32 +0700

    fix: consume utxo with datum hash

--- packages/lucid/test/specs/hello-params.ts
+++ packages/lucid/test/specs/hello-params.ts
@@ -105,3 +105,86 @@ export const collectFunds = Effect.gen(function* ($) {
     .completeProgram();
   return signBuilder;
 }).pipe(Effect.flatMap(handleSignSubmit), withLogRetry, Effect.orDie);
+
+export const depositFundsDatumHash = Effect.gen(function* () {
+  const { user } = yield* User;
+  const networkConfig = yield* NetworkConfig;
+  const publicKeyHash = getAddressDetails(
+    yield* Effect.promise(() => user.wallet().address()),
+  ).paymentCredential?.hash;
+  const datum = Data.to(new Constr(0, [publicKeyHash!]));
+  // const { contractAddress } = yield* HelloContractParams;
+  const helloCBOR = yield* pipe(
+    Effect.fromNullable(
+      scripts.validators.find(
+        (v) => v.title === "hello_world_params.hello_world_params.spend",
+      ),
+    ),
+    Effect.andThen((script) => script.compiledCode),
+  );
+  const applied = applyParamsToScript(applyDoubleCborEncoding(helloCBOR), [
+    publicKeyHash!,
+    fromText("DatumHash!"),
+  ]);
+
+  const hello: SpendingValidator = {
+    type: "PlutusV3",
+    script: applyDoubleCborEncoding(applied),
+  };
+
+  const contractAddress = validatorToAddress(networkConfig.NETWORK, hello);
+
+  const signBuilder = yield* user
+    .newTx()
+    .pay.ToAddressWithData(
+      contractAddress,
+      {
+        kind: "asHash",
+        value: datum,
+      },
+      { lovelace: 10_000_000n },
+    )
+    .completeProgram();
+  return signBuilder;
+}).pipe(Effect.flatMap(handleSignSubmit), withLogRetry, Effect.orDie);
+
+export const collectFundsDatumHash = Effect.gen(function* ($) {
+  const { user } = yield* User;
+  const networkConfig = yield* NetworkConfig;
+  const helloCBOR = yield* pipe(
+    Effect.fromNullable(
+      scripts.validators.find(
+        (v) => v.title === "hello_world_params.hello_world_params.spend",
+      ),
+    ),
+    Effect.andThen((script) => script.compiledCode),
+  );
+  const publicKeyHash = getAddressDetails(
+    yield* Effect.promise(() => user.wallet().address()),
+  ).paymentCredential?.hash;
+  const applied = applyParamsToScript(applyDoubleCborEncoding(helloCBOR), [
+    publicKeyHash!,
+    fromText("DatumHash!"),
+  ]);
+  const hello: SpendingValidator = {
+    type: "PlutusV3",
+    script: applyDoubleCborEncoding(applied),
+  };
+  const contractAddress = validatorToAddress(networkConfig.NETWORK, hello);
+  const allUtxos = yield* Effect.tryPromise(() =>
+    user.utxosAt(contractAddress),
+  );
+  const ownerUTxO = yield* Effect.fromNullable(
+    allUtxos.find((utxo) => utxo.datumHash),
+  );
+
+  const addr = yield* Effect.promise(() => user.wallet().address());
+  const redeemer = Data.to(new Constr(0, [fromText("DatumHash!")]));
+  const signBuilder = yield* user
+    .newTx()
+    .collectFrom([ownerUTxO], redeemer)
+    .attach.SpendingValidator(hello)
+    .addSigner(addr)
+    .completeProgram();
+  return signBuilder;
+}).pipe(Effect.flatMap(handleSignSubmit), withLogRetry, Effect.orDie);
