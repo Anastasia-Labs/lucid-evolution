fc3c742902e3a1026ceca3f2c4bc2fb1cfd6bf86
hadelive <liveithade@gmail.com>
2025-07-17 20:57:51 +0700

    fix: format

--- packages/uplc/src/browser/uplc_tx_bg.wasm.d.ts
+++ packages/uplc/src/browser/uplc_tx_bg.wasm.d.ts
@@ -1,8 +1,27 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const eval_phase_two_raw: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: bigint, j: bigint, k: bigint, l: bigint, m: number) => [number, number, number, number];
-export const apply_params_to_script: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const eval_phase_two_raw: (
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+  e: number,
+  f: number,
+  g: number,
+  h: number,
+  i: bigint,
+  j: bigint,
+  k: bigint,
+  l: bigint,
+  m: number,
+) => [number, number, number, number];
+export const apply_params_to_script: (
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+) => [number, number, number, number];
 export const __wbindgen_export_0: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __externref_table_alloc: () => number;
