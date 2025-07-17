4e57d8abb5cf233b02c6e568b9608328fde3ae90
hadelive <liveithade@gmail.com>
2025-07-17 20:53:39 +0700

    build: fix uplc

--- packages/uplc/src/browser/uplc_tx.d.ts
+++ packages/uplc/src/browser/uplc_tx.d.ts
@@ -1,17 +1,4 @@
 /* tslint:disable */
 /* eslint-disable */
-export function eval_phase_two_raw(
-  tx_bytes: Uint8Array,
-  utxos_bytes_x: Uint8Array[],
-  utxos_bytes_y: Uint8Array[],
-  cost_mdls_bytes: Uint8Array,
-  initial_budget_n: bigint,
-  initial_budget_d: bigint,
-  slot_config_x: bigint,
-  slot_config_y: bigint,
-  slot_config_z: number,
-): Uint8Array[];
-export function apply_params_to_script(
-  params_bytes: Uint8Array,
-  plutus_script_bytes: Uint8Array,
-): Uint8Array;
+export function eval_phase_two_raw(tx_bytes: Uint8Array, utxos_bytes_x: Uint8Array[], utxos_bytes_y: Uint8Array[], cost_mdls_bytes: Uint8Array, initial_budget_n: bigint, initial_budget_d: bigint, slot_config_x: bigint, slot_config_y: bigint, slot_config_z: number): Uint8Array[];
+export function apply_params_to_script(params_bytes: Uint8Array, plutus_script_bytes: Uint8Array): Uint8Array;
--- packages/uplc/src/browser/uplc_tx.js
+++ packages/uplc/src/browser/uplc_tx.js
@@ -2,4 +2,3 @@ import * as wasm from "./uplc_tx_bg.wasm";
 export * from "./uplc_tx_bg.js";
 import { __wbg_set_wasm } from "./uplc_tx_bg.js";
 __wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
